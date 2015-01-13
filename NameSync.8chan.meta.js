b77f02d69490d9de559c7b770719ce9a9ec0a94a
nokosage <nokosage@charter.net>
2015-01-13 12:30:09 -0500

    v0.3.3 - 8chan domain change

--- NameSync.8chan.meta.js
+++ NameSync.8chan.meta.js
@@ -1,10 +1,10 @@
 ﻿// ==UserScript==
 // @name         8chan Name Sync
-// @version      0.3.2
+// @version      0.3.3
 // @namespace    nokosage
 // @description  Enables names on 8chan. Does not require 8chan X.
 // @author       nokosage
-// @include      *://*8chan.co/b/*
+// @include      *://*8ch*/b/
 // @run-at       document-start
 // @grant        none
 // @updateURL    https://namesync.org/8chan/builds/NameSync.8chan.meta.js
